af912dd0e8228e2a92e07f1944e1cbf34698887c
maanmohit514 <95268414+maanmohit514@users.noreply.github.com>
2024-09-17 14:11:36 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+console.log(3+3)
+
+let name = "mohit"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// console.table([name,age,isLoggedIn,state])
+
+// Number
+// BigInt
+// String
+// Boolean
+// null
+// undefined
+// Symbol
+
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
