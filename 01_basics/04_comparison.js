54d678a2ce5363e96bb334dce52c0c7cc8e8470a
maanmohit514 <95268414+maanmohit514@users.noreply.github.com>
2024-09-17 15:05:39 +0000

    done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2">1)
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+
+// console.log("2"===2)
+// Used for strict checking it compares the datatype also...
+
+// console.log("2"==2)
+//It is not that much strict as it doesn't compares the datatype...
