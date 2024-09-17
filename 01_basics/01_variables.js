08302a9976dbbb3d481b3171e076d5007cda4001
maanmohit514 <95268414+maanmohit514@users.noreply.github.com>
2024-09-17 13:41:13 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14453
+let accountEmail = "maanmohit096@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+// console.log(accountId)
+// console.log(accountEmail)
+// console.log(accountPassword)
+// accountEmail = "maanmohit131@gmial.com"
+// accountPassword = "12313412"
+// console.log(accountEmail)
+// console.log(accountPassword)
+
+console.table([accountId,accountPassword,accountEmail,accountCity])
