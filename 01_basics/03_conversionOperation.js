c7e7bded792c34b5f03454dc6098628ddbb9baa9
maanmohit514 <95268414+maanmohit514@users.noreply.github.com>
2024-09-17 14:58:53 +0000

    done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score = "mohit"
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+let isLoggedIn = "mohit"
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(BooleanisLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)  
+// console.log(typeof stringNumber)
+
+// ***********************Operations*************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello"
+let str2 = "mohit"
+
+// console.log( str1 + str2)
+
+// console.log("1" + 2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// ++gameCounter
+// console.log(gameCounter)
