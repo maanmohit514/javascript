8b109546674cc5735d85a48d082a2b1d1928a657
maanmohit514 <95268414+maanmohit514@users.noreply.github.com>
2024-09-17 15:25:41 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "mohit"
+const repoCount = 50
+
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+const gameName = new String('mohit-maan')
+
+// console.log(gameName[5])
+// console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.indexOf("m"))
+
+// const newString = gameName.substring(0,4)
+// console.log(newString)
+// If start is neg it assumes it to be 0.
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
+// If start is neg it starts from the end side and works same as substring..
+
+// const newStringOne = "  mohit  "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+//Used to remove spaces from the string....
+
+// const url = "https://mohit.com/mohit%01maan"
+
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('maan'))
+// console.log(gameName.split('-'))
